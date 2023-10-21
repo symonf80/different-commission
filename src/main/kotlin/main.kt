88f930df46b31502a77f9654644a65fa39992cd1
symonf80 <94631730+symonf80@users.noreply.github.com>
2023-10-21 15:40:41 +0300

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,30 +2,17 @@
 val prevTransition = 0.0
 val currentTransition = 10_000.0
 val cardType = "Vk_Pay"
-fun main() {
-
-
-    val prevTransition = 0.0
-    val currentTransition = 10_000.0
-    val cardType = "Vk_Pay" 
+ 
 
 fun main() {
-
-    val rezultCommision = countCommision(cardType, prevTransition, currentTransition)
+   val rezultCommision = countCommision(cardType, prevTransition, currentTransition)
     if (rezultCommision == -1.0) {
         println("Превышен лимит перевода по карте $cardType")
     } else
-
-        println("�������� � ����� $currentTransition �� ����� $cardType $rezultCommision ���.")
-
-}
-
-fun countCommision(cardType: String = "Vk_Pay", prevTransition: Double = 0.0, currentTransition: Double): Number {
         println("Комиссия с суммы $currentTransition по карте $cardType $rezultCommision руб.")
 }
 
-fun countCommision(cardType: String, prevTransition: Double, currentTransition: Double): Double {
-
+fun countCommision(cardType: String = "Vk_Pay", prevTransition: Double = 0.0, currentTransition: Double): Number {
     val prevTransSum = prevTransition + currentTransition
     val limitMessage = -1.0
     val minCoast = 35.0
@@ -35,19 +22,11 @@ fun countCommision(cardType: String, prevTransition: Double, currentTransition: 
     val monthLyLimitVk = 40_000.0
     var result = when (cardType) {
         "MasterCard", "Maestro" -> if (currentTransition < 75000.0) 0.0 else (currentTransition / 100 * 0.6) + 20.0
-
-        "Visa", "���" -> if ((currentTransition / 100 * 0.75) < minCoast) minCoast else (currentTransition / 100 * 0.75)
-
         "Visa", "Мир" -> if ((currentTransition / 100 * 0.75) < minCoast) minCoast else (currentTransition / 100 * 0.75)
-
         else -> if (currentTransition > oneLimitVk || prevTransSum > monthLyLimitVk) limitMessage else 0.0
 
     }
     if (currentTransition > oneLimit || prevTransSum > monthlyLimit) result = limitMessage
-
-
-
-
     return result
 }
 
